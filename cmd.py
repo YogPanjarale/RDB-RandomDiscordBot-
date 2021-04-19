83aeba43acb1c3d147ef86a1d7528ef5cde9e6c4
YogPanjarale <yog.panjarale@gmail.com>
2021-04-19 23:16:35 +0530

    yt command

--- cmd.py
+++ cmd.py
@@ -3,43 +3,49 @@ from discord.ext import commands
 from discord.ext.commands.context import Context
 
 import requests
-from googlesearch.googlesearch import GoogleSearch, SearchResponse,SearchResult
+from googlesearch.googlesearch import GoogleSearch, SearchResponse, SearchResult
 from urllib.parse import quote
 import instatools3
-from utils.youtube_search import search
+from utils.youtube_search import searchYT
 
 
 class Cmd(commands.Cog):
     @commands.command('gh')
-    async def github(self,ctx:Context,*,term:str=''):
-        r  = requests.get(f'https://api.github.com/users/{term}').json()
+    async def github(self, ctx: Context, *, term: str = ''):
+        r = requests.get(f'https://api.github.com/users/{term}').json()
         try:
-            myEmbed=discord.Embed(title=term.title(),description=r['bio'],url=r['html_url'])
-            myEmbed.add_field(name="Public repos",value=r['public_repos'])
-            myEmbed.add_field(name="Public gists",value=r['public_gists'])
-            myEmbed.add_field(name="Followers",value=r['followers'])
-            myEmbed.add_field(name="Following",value=r['following'])
+            myEmbed = discord.Embed(
+                title=term.title(), description=r['bio'], url=r['html_url'])
+            myEmbed.add_field(name="Public repos", value=r['public_repos'])
+            myEmbed.add_field(name="Public gists", value=r['public_gists'])
+            myEmbed.add_field(name="Followers", value=r['followers'])
+            myEmbed.add_field(name="Following", value=r['following'])
             myEmbed.set_thumbnail(url=r['avatar_url'])
-            myEmbed.set_footer(text="asked By ".title()+str(ctx.author),icon_url=str(ctx.message.author.avatar_url))
+            myEmbed.set_footer(text="asked By ".title(
+            )+str(ctx.author), icon_url=str(ctx.message.author.avatar_url))
             await ctx.message.channel.send(embed=myEmbed)
         except Exception as e:
-            myEmbed= discord.Embed(title="Not Found !",description="I cound not find that user")
+            myEmbed = discord.Embed(
+                title="Not Found !", description="I cound not find that user")
             await ctx.message.channel.send(embed=myEmbed)
+
     @commands.command('ggl')
-    async def ggl(self,ctx, *, term: str=''):
+    async def ggl(self, ctx, *, term: str = ''):
         # result = search(query=term,num_results=10)
-        result:SearchResponse = GoogleSearch().search(query= term,num_results=5)
+        result: SearchResponse = GoogleSearch().search(query=term, num_results=5)
         print(term)
-        myEmbed = discord.Embed(title="Google search", description=">>>this is     what i found",color=0x00ff00,url='https://google.com/?q='+quote(term))
-        #myEmbed.set_image(pictureUrl)
-        r:SearchResult
+        myEmbed = discord.Embed(title="Google search", description=">>>this is     what i found",
+                                color=0x00ff00, url='https://google.com/?q='+quote(term))
+        # myEmbed.set_image(pictureUrl)
+        r: SearchResult
         for r in result.results:
-            myEmbed.add_field(name=r.title,inline=False,value=r.url)
-        if len(result.results)<=0:
+            myEmbed.add_field(name=r.title, inline=False, value=r.url)
+        if len(result.results) <= 0:
             myEmbed.set_footer(text="Nothing Found from the search")
         await ctx.message.channel.send(embed=myEmbed)
+
     @commands.command('insta')
-    async def insta(self,ctx, *, term: str = ''):
+    async def insta(self, ctx, *, term: str = ''):
         u = instatools3.igstalker(term)
         if u:
             print(u)
@@ -56,13 +62,14 @@ class Cmd(commands.Cog):
                 title="IG acc search", description=">>>this is what i found", color=0x00ff00)
             # myEmbed.set_image(pictureUrl)
             myEmbed.add_field(name="name of account : ",
-                            value=username, inline=False)
+                              value=username, inline=False)
             myEmbed.add_field(name="no. of followers : ",
-                            value=follower, inline=False)
-            myEmbed.add_field(name="following : ", value=following, inline=False)
+                              value=follower, inline=False)
+            myEmbed.add_field(name="following : ",
+                              value=following, inline=False)
             myEmbed.add_field(name="bio : ", value=bio, inline=False)
             myEmbed.add_field(name="link to profile : ",
-                            value=profilelink, inline=False)
+                              value=profilelink, inline=False)
 
             await ctx.message.channel.send(embed=myEmbed)
 
@@ -71,8 +78,16 @@ class Cmd(commands.Cog):
                 title="IG acc search", description="IG acc not found :(( ,retry", color=0x00ff00)
 
             await ctx.message.channel.send(embed=myEmbed8)
+
     @commands.command('yt')
-    async def youtube(self,ctx,*,term:str = ''):
-        myEmbed = discord.Embed()
+    async def youtube(self, ctx, *, term: str = ''):
+        l=searchYT(term)
+        l=l[:5]
+        for i in range(1,len(l)):
+            id=l[i]
+            await ctx.message.channel.send(f'**{i}** https://www.youtube.com/watch?v={id}')  
+        
+
+
 def setup(client):
     client.add_cog(Cmd(client))
--- utils/youtube_search.py
+++ utils/youtube_search.py
@@ -1,16 +1,8 @@
-from googleapiclient.discovery import build
-from dotenv import dotenv_values
+import urllib.request
+import urllib.parse
+import re
 
-def get_service():
-    # Get developer key from "credentials" tab of api dashboard
-    return build("youtube", "v3", developerKey="key")
-
-def search(term, channel):
-    service = get_service()
-    resp = service.search().list(
-        part="id",
-        q=term,
-        # safeSearch="none" if channel.is_nsfw() else "moderate",
-        videoDimension="2d",
-        ).execute()
-    return resp["items"][0]["id"]["videoId"]
+def searchYT(term:str):
+    html = urllib.request.urlopen("https://www.youtube.com/results?search_query="+urllib.parse.quote(term))
+    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    return list(dict.fromkeys(video_ids))

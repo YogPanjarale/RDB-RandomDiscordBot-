e0cd4ee92546f320207847968c0ef3f1cb8cd161
omegamg05111101 <omegarapid123456789@gmail.com>
2021-04-29 18:30:36 +0530

    updated fun.py to latest version

--- fun.py
+++ fun.py
@@ -6,10 +6,10 @@ from discord.member import Member
 
 class Fun(commands.Cog):
 
-    @commands.command(name='how')
+@commands.command(name='how')
 
-    async def how(self,context):
-    
+    async def how(context):
+
         myEmbed7 = discord.Embed(title="how am i?", description="im doing great , thank u for asking :))",color=0x00ff00)
         myEmbed7.add_field(name="what about you sir/ma'am?",value="how are u today ?",inline=False)
         myEmbed7.set_image(url="https://media.giphy.com/media/Xfbugf2U4rC1Db4NSq/giphy.gif")
@@ -18,9 +18,12 @@ class Fun(commands.Cog):
     # pfp
 
     @commands.command('pfp')
-    async def pfp(self,ctx, member: Member = None):
+    async def pfp(ctx, member: Member = None):
         if not member:
             member = ctx.author
+
+        await ctx.send.reply(member.avatar_url)
+
 
     @commands.command(name='say')
     async def say(self,ctx, *, term: str = ''):
@@ -37,15 +40,12 @@ class Fun(commands.Cog):
         embed.set_image(url="https://media.giphy.com/media/djRJNZqj508sE/giphy.gif")
         
         await ctx.message.reply(embed=embed)
-    
+
     @commands.command('sad')
     async def sad(self,context):
-        myEmbed = discord.Embed(
-            title="sad?", description="dont be , ur bots here for you ", color=0x00ff00)
-        myEmbed.add_field(
-            name="what caused it?", value="r u actually crying or crying internally?", inline=False)
-        myEmbed.add_field(name="friends or family?",
-                           value="doesnt matter , go vibe to some music!!", inline=False)
+        myEmbed = discord.Embed(title="sad?", description="dont be , ur bots here for you ", color=0x00ff00)
+        myEmbed.add_field(name="what caused it?", value="r u actually crying or crying internally?", inline=False)
+        myEmbed.add_field(name="friends or family?",value="doesnt matter , go vibe to some music!!", inline=False)
 
         await context.message.reply(embed=myEmbed)
 

e3ed8ac6271438c6bb36673af7cc7564c907ee4d
YogPanjarale <yog.panjarale@gmail.com>
2021-04-29 19:31:54 +0530

    `fun.py` fixed

--- fun.py
+++ fun.py
@@ -8,17 +8,17 @@ class Fun(commands.Cog):
 
     @commands.command(name='how')
 
-    async def how(context):
+    async def how(self,ctx):
 
         myEmbed7 = discord.Embed(title="how am i?", description="im doing great , thank u for asking :))",color=0x00ff00)
         myEmbed7.add_field(name="what about you sir/ma'am?",value="how are u today ?",inline=False)
         myEmbed7.set_image(url="https://media.giphy.com/media/Xfbugf2U4rC1Db4NSq/giphy.gif")
         
-        await context.message.reply(embed=myEmbed7)
+        await ctx.message.reply(embed=myEmbed7)
     # pfp
 
     @commands.command('pfp')
-    async def pfp(ctx, member: Member = None):
+    async def pfp(self,ctx, member: Member = None):
         if not member:
             member = ctx.author
 
@@ -42,12 +42,12 @@ class Fun(commands.Cog):
         await ctx.message.reply(embed=embed)
 
     @commands.command('sad')
-    async def sad(self,context):
+    async def sad(self,ctx):
         myEmbed = discord.Embed(title="sad?", description="dont be , ur bots here for you ", color=0x00ff00)
         myEmbed.add_field(name="what caused it?", value="r u actually crying or crying internally?", inline=False)
         myEmbed.add_field(name="friends or family?",value="doesnt matter , go vibe to some music!!", inline=False)
 
-        await context.message.reply(embed=myEmbed)
+        await ctx.message.reply(embed=myEmbed)
 
 def setup(client):
     client.add_cog(Fun(client))

b0699996f0fd2a75b4018ae088cb871a363f30f5
omegamg05111101 <omegarapid123456789@gmail.com>
2021-05-06 19:15:12 +0530

    updated menu

--- menu.py
+++ menu.py
@@ -35,6 +35,27 @@ class menu(commands.Cog):
 
         await context.message.reply(embed=myEmbed)
 
+    @commands.command(name='mod')
+    async def util(self, context):
+
+        myEmbed = discord.Embed(
+            title="Creator and moderation commands", description="these r one of the secondary cmds,dont forget to put prefix mg. before each of them", color=0x00ff00)
+        myEmbed.add_field(
+            name="1) re - ", value="reloads the files , re fun/mod/cmd/menu/info", inline=True)
+        myEmbed.add_field(name="2) m/um - ",
+                          value="mutes or unmutes someone", inline=True)
+        myEmbed.add_field(name="3) k - ",
+                          value="kicks someone", inline=True)
+        myEmbed.add_field(name="4) d - ",
+                          value="deletes a specific amount of msgs", inline=True)
+        myEmbed.add_field(name="5) b/ub - ",
+                          value="bans or unbans someone", inline=True)
+        myEmbed.add_field(name="6) ld/rld - ",
+                          value="puts a person in lockdown , i.e , they cant see or text messages in any channel of the server", inline=True)
+        myEmbed.add_field(name="7) dc - ",
+                          value="deletes a specific channel of the server - use carefully", inline=True)
+        await context.message.reply(embed=myEmbed)
+
     @commands.command(name='fun')
     async def fun(self, context):
 
@@ -46,6 +67,10 @@ class menu(commands.Cog):
         myEmbed.add_field(name="4) how - ", value="fun cmd4", inline=True)
         myEmbed.add_field(name="5) pfp - ", value="fun cmd5", inline=True)
         myEmbed.add_field(name="6) fight - ", value="fun cmd6", inline=True)
+        myEmbed.add_field(name="7) 8b - ", value="fun cmd6", inline=True)
+        myEmbed.add_field(name="8) spam - ", value="fun cmd6", inline=True)
+        myEmbed.add_field(name="9) count - ", value="fun cmd6", inline=True)
+        myEmbed.add_field(name="10) custom command (cc) - ", value="fun cmd6", inline=True)
 
         await context.message.reply(embed=myEmbed)
 

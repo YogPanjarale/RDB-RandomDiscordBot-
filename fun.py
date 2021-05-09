9e59179bd59cf76d0b4508f06c573987be248ae3
YogPanjarale <yog.panjarale@gmail.com>
2021-05-09 22:21:25 +0530

    search commands by user implemented

--- fun.py
+++ fun.py
@@ -1,4 +1,4 @@
-from my_utils.my_cc import addCC, searchCC
+from my_utils.my_cc import addCC, searchCC, serchCCByUser
 import discord
 from discord.ext import commands
 from discord.ext.commands.bot import Bot
@@ -14,6 +14,24 @@ class Fun(commands.Cog):
         self.bot:Bot     = bot
     @commands.command(name="cc")
     async def cc(self,ctx:Context,name:str=" ",*description):
+        if name.startswith("<@"):
+            r = serchCCByUser(name.replace("!",""))
+            if r:
+                # print("====================")
+                # print("r=")
+                # print(r)
+                # print("====================")
+                myEmbed = discord.Embed(name=f"Commands Created By {name}",description=f"{name} has created{len(r)+1} Commands ")
+                for i in r:
+                    # print("====================")
+                    # print("i=")
+                    # print(i)
+                    # print("====================")
+                    des = i['description'] + "\nC*reated At " + i["time-created"]+"*"
+                    myEmbed.add_field(name=i["name"],value=des,inline=False)
+                return await ctx.channel.send(embed = myEmbed)
+            else:
+                myEmbed = discord.Embed(name=f"Commands Created By {name}",description=f"{name} has not created any Commands")
         if name == " ":
             return await ctx.channel.send("You did not provide the command name!")
         if not description :
@@ -21,7 +39,7 @@ class Fun(commands.Cog):
         if name in self.bot.all_commands:
             return await ctx.channel.send(f"{name} command already exists!")
         description=' '.join(description)
-        addCC(name=name,description=description)
+        addCC(name=name,description=description,user=ctx.author.mention)
         await ctx.channel.send(f"Command with name : `{name}` , and description : `{description}` has been added by {ctx.author.mention}")
     @commands.Cog.listener()
     async def on_command_error(self,ctx:Context,error):
--- my_utils/my_cc.py
+++ my_utils/my_cc.py
@@ -1,10 +1,11 @@
 from tinydb import TinyDB,Query
+import datetime;
 db = TinyDB('./db.json')
 q=Query()
-def addCC(name:str,description:str):
-    db.insert({"name":name,"description":description})
+def addCC(name:str,description:str,user:str):
+    db.insert({"name":name,"description":description,"user":user,"time-created":f"{datetime.datetime.now()}"})
     # print(db.all())
-    print(f"Command with name : {name}, and description : {description} Added")
+    print(f"Command with name : {name}, and description : {description} Added, by user {user}")
     pass
 def searchCC(name:str):
     print(f"Searching for {name}")
@@ -13,3 +14,9 @@ def searchCC(name:str):
     if len(result)>0:
         return result[0]
     pass
+v = list[dict]
+def serchCCByUser(user:str)->v:
+    print(f"Searching for CC made by user {user}")
+    result=db.search(q.user ==user)
+    print(result)
+    return result
